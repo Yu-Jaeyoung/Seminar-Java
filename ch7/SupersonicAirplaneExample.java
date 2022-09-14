06800cca47c50ab31dc60d1065bd0c695eecbb7b
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-09-14 20:47:51 +0900

    오류 수정

--- ch7/SupersonicAirplaneExample.java
+++ ch7/SupersonicAirplaneExample.java
@@ -6,15 +6,10 @@ public class SupersonicAirplaneExample {
         sa.takeOff();
         sa.fly();
         sa.flyMode = SupersonicAirplane.SUPERSONIC;
-<<<<<<< HEAD
-        System.out.println(sa.flyMode);
         sa.fly();
         sa.flyMode = SupersonicAirplane.NORMAL;
-        System.out.println(sa.flyMode);
-=======
         sa.fly();
         sa.flyMode = SupersonicAirplane.NORMAL;
->>>>>>> 11e6a59 (update)
         sa.fly();
         sa.land();
     }
