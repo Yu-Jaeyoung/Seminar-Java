bdc42936f2eeade5406afaf847134dff5fecb1ba
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-09-21 21:34:53 +0900

    chap09 chekcout No.5

---
+++ src/ch9/CheckAnonymousExample.java
@@ -0,0 +1,17 @@
+package ch9;
+
+public class CheckAnonymousExample {
+    public static void main(String[] args) {
+        CheckAnonymous anony = new CheckAnonymous();
+        anony.field.run();
+        anony.method1();
+        anony.method2(
+                new Vehicle() {
+                    @Override
+                    public void run() {
+                        System.out.println("Truck is moving.");
+                    }
+                }
+        );
+    }
+}
