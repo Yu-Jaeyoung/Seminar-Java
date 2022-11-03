2793809397ca209cb50d2b9a953e1f0cf39cdfdc
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-11-04 02:25:05 +0900

    ch13 study

---
+++ src/ch13/Container.java
@@ -0,0 +1,29 @@
+package ch13;
+
+/*public class Container<T> {
+    private T t;
+
+    public T get() {
+        return t;
+    }
+
+    public void set(T t){
+        this.t = t;
+    }
+}*/
+
+public class Container<K,V> {
+    private K key;
+    private V value;
+
+    public K getKey() {
+        return this.key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+
+    public void set(K key,V value) { this.key = key; this.value = value; }
+
+}
---
+++ src/ch13/Person.java
@@ -0,0 +1,14 @@
+package ch13;
+
+public class Person {
+    String name;
+    public Person(String name) {
+        this.name = name;
+    };
+    public String getName() {return name;}
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
