8f2b4dbe7e060f86b0af61c10e36b49becb6bab2
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-11-11 12:07:57 +0900

    studying ch15-2

---
+++ src/ch15/ComparableExample.java
@@ -0,0 +1,21 @@
+package ch15;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class ComparableExample {
+    public static void main(String[] args) {
+        TreeSet<Person> treeSet = new TreeSet<Person>();
+
+        treeSet.add(new Person("홍길동", 45));
+        treeSet.add(new Person("감자바", 25));
+        treeSet.add(new Person("박지원", 31));
+        // 저장될 때 나이 순으로 정렬
+
+        Iterator<Person> iterator = treeSet.iterator();
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            System.out.println(person.name + ":" + person.age);
+        } // 왼쪽 마지막 노드에서 오른쪽 마지막 노드까지 반복해서 가져오기 (오름차순)
+    }
+}
