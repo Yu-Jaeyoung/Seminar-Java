fc03b23d7103e80770011d017fdcb893af9ad947
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-11-14 16:50:52 +0900

    studying ch18

---
+++ src/ch18/SystemExample2.java
@@ -0,0 +1,11 @@
+package ch18;
+
+import java.io.*;
+
+public class SystemExample2 {
+    public static void main(String[] args) throws Exception{
+        InputStream is = System.in;
+
+        byte[ ] datas = new byte[100];
+    }
+}
