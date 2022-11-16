88da720f7b42e616e3a785ce6be7a667943d3d93
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-11-16 20:21:00 +0900

    studying ch18-4

--- src/ch18/ServerExample.java
+++ src/ch18/ServerExample.java
@@ -1,6 +1,6 @@
 package ch18;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 
 public class ServerExample {
@@ -14,6 +14,26 @@ public class ServerExample {
                 Socket socket = serverSocket.accept(); // 클라이언트 연결 수락
                 InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
                 System.out.println("[연결 수락함] " + isa.getHostName());
+
+                byte[] bytes = null;
+                String message = null;
+
+                InputStream is = socket.getInputStream();
+                bytes = new byte[100];
+                int readByteCount = is.read(bytes);
+                message = new String(bytes, 0, readByteCount, "UTF-8");
+                System.out.println("[데이터 받기 성공] " + message);
+
+                OutputStream os = socket.getOutputStream();
+                message = "Hello Client";
+                bytes = message.getBytes("UTF-8");
+                os.write(bytes);
+                os.flush();
+                System.out.println("[데이터 보내기 성공]");
+
+                is.close();
+                os.close();
+                socket.close();
             }
         } catch (Exception e) {
         }
