8af0cd8135edd4037ace248c9cf7f41ba65629ba
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-11-27 20:54:38 +0900

    studying ch16

---
+++ src/ch16/FromFileContentExample.java
@@ -0,0 +1,30 @@
+package ch16;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FromFileContentExample {
+    public static void main(String[] args) throws IOException{
+        Path path = Paths.get("src/ch16/linedata.txt"); // 파일 경로 정보를 가지고 있는 Path 생성
+        Stream<String> stream;
+
+        // Files.lines() 메소드 이용
+        stream = Files.lines(path, Charset.defaultCharset()); // 운영체제의 기본 문자 셋
+        stream.forEach( System.out :: println); // s-> System.out.println(s)와 동일
+        System.out.println();
+
+        //BufferedReader의 lines() 메소드 이용
+        File file = path.toFile();
+        FileReader fileReader = new FileReader(file);
+        BufferedReader br = new BufferedReader(fileReader);
+        stream = br.lines();
+        stream.forEach(System.out :: println);
+    }
+}
---
+++ src/ch16/LambdaExpressionsExample.java
@@ -0,0 +1,22 @@
+package ch16;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LambdaExpressionsExample {
+    public static void main(String[] args) {
+        List<Student> list = Arrays.asList(
+                new Student("홍길동", 90),
+                new Student("신용권", 92)
+        );
+
+        Stream<Student> stream = list.stream(); // 스트림 얻기
+        // List 컬렉션에서 Student를 가져와 람다식의 매개값으로 제공
+        stream.forEach(s -> {
+            String name = s.getName();
+            int score = s.getScore();
+            System.out.println(name + "-" + score);
+        });
+    }
+}
