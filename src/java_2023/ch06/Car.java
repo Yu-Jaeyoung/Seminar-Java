b677dfc5b9736f1cb3deee9a09c8825385130c1a
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-12-31 18:56:42 +0900

    Field Example

---
+++ src/java_2023/ch06/Car.java
@@ -0,0 +1,10 @@
+package java_2023.ch06;
+
+public class Car {
+    // 필드
+    String company = "현대 자동차";
+    String model = "그랜저";
+    String color = "검정";
+    int maxSpeed = 350;
+    int speed;
+}
