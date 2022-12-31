4c6066afd1cee599e4821c34a9af801efb3667ae
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-12-31 22:42:42 +0900

    Singleton Example

---
+++ src/java_2023/ch06/Singleton.java
@@ -0,0 +1,13 @@
+package java_2023.ch06;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {
+    }
+
+    static Singleton getInstance() {
+        return singleton;
+    }
+}
+
