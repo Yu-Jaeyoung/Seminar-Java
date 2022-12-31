631d5bb8b7343e548ecbf7d8b9e713565dd98028
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-12-31 18:49:16 +0900

    Class Example

---
+++ src/java_2023/ch06/Student.java
@@ -0,0 +1,4 @@
+package java_2023.ch06;
+
+public class Student {
+}
---
+++ src/java_2023/ch06/StudentExample.java
@@ -0,0 +1,11 @@
+package java_2023.ch06;
+
+public class StudentExample {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        System.out.println("s1 변수가 Student 객체를 참조");
+
+        Student s2 = new Student();
+        System.out.println("s2 변수가 또 다른 Student 객체를 참조");
+    }
+}
