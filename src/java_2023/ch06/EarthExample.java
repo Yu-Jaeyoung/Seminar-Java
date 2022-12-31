1b24f868c68723249c50b74c39288d6d7067edd0
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-12-31 23:04:41 +0900

    Static Final Example

---
+++ src/java_2023/ch06/EarthExample.java
@@ -0,0 +1,8 @@
+package java_2023.ch06;
+
+public class EarthExample {
+    public static void main(String[] args) {
+        System.out.println("지구의 반지름 : " + Earth.EARTH_RADIUS + " km");
+        System.out.println("지구의 표먼적 : " + Earth.EARTH_SURFACE_AREA + " km^2");
+    }
+}
