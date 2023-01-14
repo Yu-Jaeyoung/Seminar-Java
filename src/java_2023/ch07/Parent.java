e4f3e9d5959425e4674f376c0cf492aea77786ec
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-15 08:55:47 +0900

    promotion Example

---
+++ src/java_2023/ch07/Parent.java
@@ -0,0 +1,11 @@
+package java_2023.ch07;
+
+public class Parent {
+    public void method1() {
+        System.out.println("Parent-method1()");
+    }
+
+    public void method2() {
+        System.out.println("Parent-method2()");
+    }
+}
