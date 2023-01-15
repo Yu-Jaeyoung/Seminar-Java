0cb0ad3370373d9d76b044a76c1fc7d371995e00
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-15 16:03:07 +0900

    static method use Example

--- src/java_2023/ch08/RemoteControlExample.java
+++ src/java_2023/ch08/RemoteControlExample.java
@@ -2,15 +2,6 @@ package java_2023.ch08;
 
 public class RemoteControlExample {
     public static void main(String[] args) {
-
-        RemoteControl rc = null; // 인터페이스 변수 선언
-
-        rc = new Television(); // Television 객체를 인터페이스 타입에 대입
-        rc.turnOn(); // 인터페이스의 메소드 호출
-        rc.setMute(true);
-
-        rc = new Audio(); // Audio 객체를 인터페이스 타입에 대입
-        rc.turnOn(); // 인터페이스의 메소드 호출
-        rc.setMute(true);
+        RemoteControl.changeBattery();
     }
 }
