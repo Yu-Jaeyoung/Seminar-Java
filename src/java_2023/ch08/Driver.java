75bd5d9579fbf33cb8018a758931a95406c39377
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-15 16:50:16 +0900

    parameter polymorphism Example

---
+++ src/java_2023/ch08/Driver.java
@@ -0,0 +1,7 @@
+package java_2023.ch08;
+
+public class Driver {
+    public void drive(Vehicle vehicle){
+        vehicle.run();
+    }
+}
---
+++ src/java_2023/ch08/Taxi.java
@@ -0,0 +1,8 @@
+package java_2023.ch08;
+
+public class Taxi implements Vehicle {
+    @Override
+    public void run() {
+        System.out.println("택시가 달립니다.");
+    }
+}
