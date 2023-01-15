e794d9466a0cb11390a8458f47f3962a1c7abb6e
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-15 14:45:00 +0900

    anonymous object Example

--- src/java_2023/ch08/RemoteControllerExample.java
+++ src/java_2023/ch08/RemoteControllerExample.java
@@ -2,8 +2,20 @@ package java_2023.ch08;
 
 public class RemoteControllerExample {
     public static void main(String[] args) {
-        RemoteControl rc;
-        rc = new Television();
-        rc = new Audio();
+        RemoteControl rc = new RemoteControl() {
+            public void turnOn() {
+                // 실행문
+            }
+
+            public void turnOff() {
+                // 실행문
+            }
+
+            public void setVolume(int Volume) {
+                // 실행문
+            }
+        };
+
+
     }
 }
