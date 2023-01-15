d6b49550c559e6c842defda39b8e4dac3ad459a0
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-15 13:18:30 +0900

    default method Example

--- src/java_2023/ch08/RemoteControl.java
+++ src/java_2023/ch08/RemoteControl.java
@@ -7,6 +7,17 @@ public interface RemoteControl {
 
     // 추상 메소드
     public void turnOn();
+
     public void turnOff();
+
     public void setVolume(int Volume);
+
+    // 디폴트 메소드
+    default void setMute(boolean mute) {
+        if (mute) {
+            System.out.println("무음 처리합니다.");
+        } else {
+            System.out.println("무음 해제합니다.");
+        }
+    }
 }
