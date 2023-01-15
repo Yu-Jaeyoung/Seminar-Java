2854f1c7130b4cb7fbcac05ebdd678dbb47185f5
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-15 17:03:21 +0900

    instanceof Example

--- src/java_2023/ch08/Driver.java
+++ src/java_2023/ch08/Driver.java
@@ -1,7 +1,11 @@
 package java_2023.ch08;
 
 public class Driver {
-    public void drive(Vehicle vehicle){
+    public void drive(Vehicle vehicle) {
+        if (vehicle instanceof Bus) {
+            Bus bus = (Bus) vehicle;
+            bus.checkFare();
+        }
         vehicle.run();
     }
 }
