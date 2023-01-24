7477b439b64d8aa4602d1bb7ffb5ce9879d3c1c3
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-24 14:44:11 +0900

    button-event process with anonymous implement object example

---
+++ src/java_2023/ch09/Main2.java
@@ -0,0 +1,9 @@
+package java_2023.ch09;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.button1.touch();
+        window.button2.touch();
+    }
+}
