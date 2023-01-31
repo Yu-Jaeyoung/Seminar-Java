19d4c743e71b1709854b7133da02ee75cdc89285
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-01-31 23:40:05 +0900

    class cast exception example

---
+++ src/java_2023/ch10/ClassCastExceptionExample.java
@@ -0,0 +1,26 @@
+package java_2023.ch10;
+
+public class ClassCastExceptionExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        changeDog(dog);
+
+        Cat cat = new Cat();
+        changeDog(cat);
+    }
+
+    public static void changeDog(Animal animal) {
+        //if (animal instanceof Dog) {
+          Dog dog = (Dog) animal;
+        //}
+    }
+}
+
+class Animal {
+}
+
+class Dog extends Animal {
+}
+
+class Cat extends Animal {
+}
