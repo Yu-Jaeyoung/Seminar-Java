f5a0e1772e2b6d9483ce5dedc398790fcced28f1
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-01 00:50:55 +0900

    throws example

---
+++ src/java_2023/ch10/ThrowsExample.java
@@ -0,0 +1,21 @@
+package java_2023.ch10;
+
+public class ThrowsExample {
+    public static void main(String[] args) {
+        try {
+            findClass();
+        } catch (ClassNotFoundException e) {
+            System.out.println("클래스가 존재하지 않습니다.");
+        }
+    }
+
+    /*
+    public static void main(String[] args) throws ClassNotFoundException {
+        findClass();
+    }
+    */
+
+    public static void findClass() throws ClassNotFoundException {
+        Class clazz = Class.forName("java.lang.String2");
+    }
+}
