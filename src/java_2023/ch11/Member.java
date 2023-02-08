f76761bbbed18e36cbc7f6f0bf817a09f856663e
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-08 21:49:19 +0900

    Object equals() method example

---
+++ src/java_2023/ch11/Member.java
@@ -0,0 +1,20 @@
+package java_2023.ch11;
+
+public class Member {
+    public String id;
+
+    public Member(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Member) {
+            Member member = (Member) obj;
+            if (id.equals(member.id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
