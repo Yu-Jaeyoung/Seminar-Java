92b6a32ca9cf816b019ca2fa86e1bfc48c845a2d
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-08 22:01:15 +0900

    Object hashCode() example

--- src/java_2023/ch11/Member.java
+++ src/java_2023/ch11/Member.java
@@ -17,4 +17,9 @@ public class Member {
         }
         return false;
     }
+
+    @Override // id가 동일한 문자열인 경우 같은 해시 코드를 리턴
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
