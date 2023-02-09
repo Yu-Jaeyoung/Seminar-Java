b95ade20f4ff5e577ad72326da679586870208ca
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-09 17:04:46 +0900

    Object Class toString() example

---
+++ src/java_2023/ch11/SmartPhone.java
@@ -0,0 +1,16 @@
+package java_2023.ch11;
+
+public class SmartPhone {
+    private String company;
+    private String os;
+
+    public SmartPhone(String company, String os) {
+        this.company = company;
+        this.os = os;
+    }
+
+    @Override
+    public String toString() {
+        return company + ", " + os;
+    }
+}
---
+++ src/java_2023/ch11/SmartPhoneExample.java
@@ -0,0 +1,12 @@
+package java_2023.ch11;
+
+public class SmartPhoneExample {
+    public static void main(String[] args) {
+        SmartPhone myPhone = new SmartPhone("구글", "안드로이드");
+
+        String strObj = myPhone.toString();
+        System.out.println(strObj);
+
+        System.out.println(myPhone);
+    }
+}
