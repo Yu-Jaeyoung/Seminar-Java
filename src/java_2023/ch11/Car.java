712170426b193c13316f0f710a6cf69f0ed6db88
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-10 01:52:46 +0900

    Object Class deep-clone() example

---
+++ src/java_2023/ch11/Car.java
@@ -0,0 +1,9 @@
+package java_2023.ch11;
+
+public class Car {
+    public String model;
+
+    public Car(String model) {
+        this.model = model;
+    }
+}
--- src/java_2023/ch11/Member.java
+++ src/java_2023/ch11/Member.java
@@ -1,25 +1,39 @@
 package java_2023.ch11;
 
-public class Member implements Cloneable {
-    public String id;
-    public String name;
-    public String password;
-    public int age;
-    public boolean adult;
+import java.util.Arrays;
 
-    public Member(String id, String name, String password, int age, boolean adult) {
-        this.id = id;
+public class Member implements Cloneable {
+
+    public String name;
+    public int age;
+    public int[] scores;
+    public Car car;
+
+    public Member(String name, int age, int[] scores, Car car) {
         this.name = name;
-        this.password = password;
         this.age = age;
-        this.adult = adult;
+        this.scores = scores;
+        this.car = car;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // 먼저 얕은 복사를 해서 name, age를 복제한다.
+        Member cloned = (Member) super.clone(); // Object의 clone() 호출
+        // scores를 깊은 복제한다.
+        cloned.scores = Arrays.copyOf(this.scores, this.scores.length);
+        // car을 깊은 복제한다.
+        cloned.car = new Car(this.car.model);
+        // 깊은 복제된 Member 객체를 리턴
+        return cloned;
     }
 
     public Member getMember() {
         Member cloned = null;
         try {
-            cloned = (Member) clone();
-        } catch (CloneNotSupportedException e) { }
+            cloned = (Member) clone(); // 재정의된 clone() 메소드 호출
+        } catch (CloneNotSupportedException e) {
+        }
         return cloned;
     }
 }
