f0319df5f93fddc03fda4f3820f3083cdde86e1e
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-11 11:38:01 +0900

    Object Class compare() example

---
+++ src/java_2023/ch11/StudentComparator.java
@@ -0,0 +1,17 @@
+package java_2023.ch11;
+
+import java.util.Comparator;
+
+class StudentComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student a, Student b) {
+
+        if(a.sno<b.sno)
+            return -1;
+        else if (a.sno== b.sno)
+            return 0;
+        else
+            return 1;
+    }
+
+}
