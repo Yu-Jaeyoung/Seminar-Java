a37fce98cd67de672759a8c4ed948d6e71578cae
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-11 14:53:22 +0900

    Class Class Reflection Example

--- src/java_2023/ch11/Car.java
+++ src/java_2023/ch11/Car.java
@@ -2,8 +2,25 @@ package java_2023.ch11;
 
 public class Car {
     public String model;
+    public String owner;
 
     public Car(String model) {
         this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 }
