0e95eff66773101ae76279ca638ec02aabd6d591
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-11 12:07:54 +0900

    Object Class toString() example

--- src/java_2023/ch11/ToStringExample.java
+++ src/java_2023/ch11/ToStringExample.java
@@ -1,12 +1,14 @@
 package java_2023.ch11;
 
-import java.util.Date;
+import java.util.Objects;
 
 public class ToStringExample {
     public static void main(String[] args) {
-        Object obj1 = new Object();
-        Date obj2 = new Date();
-        System.out.println(obj1.toString());
-        System.out.println(obj2.toString());
+        String str1 = "홍길동";
+        String str2 = null;
+
+        System.out.println(Objects.toString(str1));
+        System.out.println(Objects.toString(str2));
+        System.out.println(Objects.toString(str2, "이름이 없습니다."));
     }
 }
