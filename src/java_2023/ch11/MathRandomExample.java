94de2aec4575fdb02f1ebd16cd21bb42e9857660
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-13 00:06:20 +0900

    Math Class example

---
+++ src/java_2023/ch11/MathRandomExample.java
@@ -0,0 +1,8 @@
+package java_2023.ch11;
+
+public class MathRandomExample {
+    public static void main(String[] args) {
+        int num = (int) (Math.random() * 6) + 1;
+        System.out.println("주사위 눈: " + num);
+    }
+}
