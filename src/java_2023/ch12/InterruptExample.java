c323cfb22a57b40ed49dc0504a906b9a83fc52cf
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-19 23:54:10 +0900

    thread safe stop : stop flag, interrupt() example

---
+++ src/java_2023/ch12/InterruptExample.java
@@ -0,0 +1,15 @@
+package java_2023.ch12;
+
+public class InterruptExample {
+    public static void main(String[] args) {
+        Thread thread = new PrintThread2();
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        thread.interrupt(); // 스레드를 종료시키기 위해 InterruptedException을 발생 시킴}
+    }
+}
