2a5c9295e2c59abc81b5fd436027c02e6bcb5dc1
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-19 21:31:39 +0900

    thread state join() example

---
+++ src/java_2023/ch12/JoinExample.java
@@ -0,0 +1,15 @@
+package java_2023.ch12;
+
+public class JoinExample {
+    public static void main(String[] args) {
+        SumThread sumThread = new SumThread();
+        sumThread.start();
+
+        /*try {
+            sumThread.join();
+        } catch (InterruptedException e) {
+
+        }*/
+        System.out.println("1~100 합 : " + sumThread.getSum());
+    }
+}
