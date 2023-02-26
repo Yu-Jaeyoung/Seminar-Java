fe2fafababe65cf10ed193e27093f8f0e7cbae73
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-27 00:55:47 +0900

    predicate functional interface example

--- src/java_2023/ch14/Student.java
+++ src/java_2023/ch14/Student.java
@@ -2,24 +2,20 @@ package java_2023.ch14;
 
 public class Student {
     private String name;
-    private int englishScore;
-    private int mathScore;
+    private String sex;
+    private int score;
 
-    public Student(String name, int englishScore, int mathScore) {
+    public Student(String name, String sex, int score){
         this.name = name;
-        this.englishScore = englishScore;
-        this.mathScore = mathScore;
+        this.sex = sex;
+        this.score = score;
     }
 
-    public String getName() {
-        return name;
+    public String getSex() {
+        return sex;
     }
 
-    public int getEnglishScore() {
-        return englishScore;
-    }
-
-    public int getMathScore() {
-        return mathScore;
+    public int getScore() {
+        return score;
     }
 }
