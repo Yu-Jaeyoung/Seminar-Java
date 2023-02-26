4073e21d2be8b880d8a46db8d40605f76852c701
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-27 01:58:04 +0900

    method references example

---
+++ src/java_2023/ch14/Calculator.java
@@ -0,0 +1,11 @@
+package java_2023.ch14;
+
+public class Calculator {
+    public static int staticMethod(int x, int y) {
+        return x + y;
+    }
+
+    public int instanceMethod(int x, int y) {
+        return x + y;
+    }
+}
--- src/java_2023/ch14/Member.java
+++ src/java_2023/ch14/Member.java
@@ -3,23 +3,23 @@ package java_2023.ch14;
 public class Member {
     private String name;
     private String id;
-    private Address address;
 
-    public Member(String name, String id, Address address) {
-        this.name = name;
-        this.id = id;
-        this.address = address;
+    public Member() {
+        System.out.println("Member() 실행");
     }
 
-    public String getName() {
-        return name;
+    public Member(String id) {
+        System.out.println("Member(String id) 실행");
+        this.id = id;
+    }
+
+    public Member(String name, String id) {
+        System.out.println("Member(String name, String id");
+        this.name = name;
+        this.id = id;
     }
 
     public String getId() {
         return id;
-    }
-
-    public Address getAddress() {
-        return address;
     }
 }
