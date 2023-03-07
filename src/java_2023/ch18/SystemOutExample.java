9483b7c79132907c9e361ca5d0595cf2c6edd4c8
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-03-07 10:41:03 +0900

    console input/output example

---
+++ src/java_2023/ch18/SystemOutExample.java
@@ -0,0 +1,25 @@
+package java_2023.ch18;
+
+import java.io.OutputStream;
+
+public class SystemOutExample {
+    public static void main(String[] args) throws Exception{
+        OutputStream os = System.out;
+
+        for (byte b = 48; b < 58; b++) {
+            os.write(b); // 아스키 코드 48~57까지의 문자를 출력
+        }
+        os.write(10); // 라인피드(10)를 출력하면 다음 행으로 넘어감
+
+        for (byte b = 97; b < 123; b++) {
+            os.write(b);
+        }
+        os.write(10);
+
+        String hangul = "가나다라마바사아자차카타파하";
+        byte[] hangulBytes = hangul.getBytes();
+        os.write(hangulBytes);
+
+        os.flush();
+    }
+}
