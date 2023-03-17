2814241e097ef90a11211e3e7bf9d1f80e4ed3b4
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-03-18 01:38:51 +0900

    collect example

--- src/java_2023/ch16/Student.java
+++ src/java_2023/ch16/Student.java
@@ -1,12 +1,32 @@
 package java_2023.ch16;
 
-public class Student implements Comparable<Student>{
-    private String name = "";
-    private int score = 0;
+public class Student implements Comparable<Student> {
+
+    public enum Sex {MALE, FEMALE}
+
+    public enum City {Seoul, Pusan}
+
+    private String name;
+    private int score;
+    private Sex sex;
+    private City city;
 
     public Student(String name, int score) {
         this.name = name;
         this.score = score;
+    }
+
+    public Student(String name, int score, Sex sex) {
+        this.name = name;
+        this.score = score;
+        this.sex = sex;
+    }
+
+    public Student(String name, int score, Sex sex, City city) {
+        this.name = name;
+        this.score = score;
+        this.sex = sex;
+        this.city = city;
     }
 
     public String getName() {
@@ -17,8 +37,12 @@ public class Student implements Comparable<Student>{
         return score;
     }
 
+    public Sex getSex() { return sex; }
+
+    public City getCity() { return city; }
+
     @Override
-    public int compareTo(Student o){
+    public int compareTo(Student o) {
         return Integer.compare(score, o.score);
     }
 }
