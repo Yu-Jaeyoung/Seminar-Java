55d86c5c5e654079544a53b39da09724c56c58b3
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-03-17 23:41:02 +0900

    sorted example

--- src/java_2023/ch16/Student.java
+++ src/java_2023/ch16/Student.java
@@ -1,6 +1,6 @@
 package java_2023.ch16;
 
-public class Student {
+public class Student implements Comparable<Student>{
     private String name = "";
     private int score = 0;
 
@@ -15,5 +15,10 @@ public class Student {
 
     public int getScore() {
         return score;
+    }
+
+    @Override
+    public int compareTo(Student o){
+        return Integer.compare(score, o.score);
     }
 }
